b35bae0e171fc8c82d246ba4d0ff6384859bf1f9
Martin Lora <martinlorajorge@gmail.com>
2023-07-10 13:30:18 -0300

    JWT, Tokens, UsersTokens

---
+++ UniversityApiBackend/Models/DataModels/JwtSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace UniversityApiBackend.Models.DataModels
+{
+    public class JwtSettings
+    {
+        //KEY
+        public bool ValidateIssuerSigningKey { get; set; }
+
+        public string? IssuerSiningKey { get; set; }
+
+
+        //Isuuer
+        public bool ValidateIssuer { get; set; } = true;
+
+        public string? ValidIssuer { get; set; }
+
+
+        //Audience
+        public bool ValidateAudience { get; set; } = true;
+
+        public string? ValidAudience { get; set; }
+
+
+        //Expiration
+        public bool ExpirationTime { get; set; }
+
+        public bool ValidateLifetime { get; set; } = true;
+
+
+    }
+}
