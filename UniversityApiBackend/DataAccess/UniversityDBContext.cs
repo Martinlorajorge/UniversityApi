7562b95a1c8bb2b1fef1f24961d23378dbc9eeb3
Martin Lora <martinlorajorge@gmail.com>
2023-07-21 21:33:19 -0300

    Log UniversityDBContext

--- UniversityApiBackend/DataAccess/UniversityDBContext.cs
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -5,8 +5,13 @@ namespace UniversityApiBackend.DataAccess
 {
     public class UniversityDBContext: DbContext
     {
-        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        //Registrar el CRUD en un logger 
+        private readonly ILoggerFactory _loggerFactory;
+
+ 
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory): base(options)
         { 
+            _loggerFactory = loggerFactory;
         }
 
         //Add DbSet (Esto va a crear Tablas de la base de datos)
@@ -20,6 +25,25 @@ namespace UniversityApiBackend.DataAccess
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Student> Students { get; set; }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+            //las dos lineas de abajo muestrn toda la informacion
+            //optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }));
+            //optionsBuilder.EnableSensitiveDataLogging();
+
+
+            //Este controla mejor la salida del log por la informacion
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+                ;
+        }
+
 
     }
 }
